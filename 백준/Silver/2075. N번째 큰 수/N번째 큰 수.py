5a0d1c813b12f07dd6a1b3bb8d9cde9e1ee55bca
galler-ist <37127971+galler-ist@users.noreply.github.com>
2024-03-14 10:57:16 +0900

    [Silver II] Title: N번째 큰 수, Time: 612 ms, Memory: 137448 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
@@ -1,14 +1,21 @@
 import sys
 import heapq
+from collections import deque
 N = int(input())
-myheap=[]
-j=0
-for _ in range(N):
-    numbers=list(map(int,sys.stdin.readline().split()))
-    for num in numbers:
-        if len(myheap)<N:
-            heapq.heappush(myheap, num)
-        else:
-            my_min=heapq.heappop(myheap)
-            heapq.heappush(myheap,max(my_min,num))
-print(heapq.heappop(myheap))
+arr = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
+rheap=list((arr[j][0] for j in range(N)))
+def bigger(i):
+    global rheap,arr
+    for j in range(N-1,-1,-1):
+        want = heapq.heappop(rheap)
+        if want>arr[j][i]:
+            heapq.heappush(rheap,want)
+            return
+        heapq.heappush(rheap,arr[j][i])
+    return
+    
+
+
+for i in range(1,N):
+    bigger(i)
+print(heapq.heappop(rheap))
