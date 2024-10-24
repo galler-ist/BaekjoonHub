13a9c0672874809bb2870c4c51e2f5ca36fc92ea
galler-ist <37127971+galler-ist@users.noreply.github.com>
2024-10-25 04:32:23 +0900

    [Gold V] Title: 평범한 배낭, Time: 5760 ms, Memory: 229084 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,12 @@
+N, K = map(int,input().split())
+bag = [list(map(int,input().split())) for _ in range(N)]
+dp = [[0]*(K+1) for _ in range(N+1)] # 행: 가방 개수, 열: 무게 오름차순
+
+for i in range(1,N+1):
+    for j in range(1, K+1):
+        w,v = bag[i-1]
+        if j<w:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w]+v)
+print(dp[-1][-1])
