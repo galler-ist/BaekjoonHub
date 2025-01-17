1adc97b4070a76c1c3c8a1788b1e6c08ee6c11a4
galler-ist <37127971+galler-ist@users.noreply.github.com>
2025-01-17 21:02:47 +0900

    [Gold II] Title: 직각다각형, Time: 692 ms, Memory: 65416 KB -BaekjoonHub

--- 백준/Gold/17611. 직각다각형/직각다각형.py
+++ 백준/Gold/17611. 직각다각형/직각다각형.py
@@ -1,30 +1,41 @@
-N = int(input())
+import sys
+input = sys.stdin.readline
+n = int(input())
 
-arr = []
-for _ in range(N):
-    x, y = map(int, input().split())
-    arr.append((x, y))
-
-MID = 500_000
-TOTAL = MID * 2 + 1
-H, V = 0, 1
-
-def solve(hv):
-    line = [0] * TOTAL
-    for cur in range(hv, N, 2):
-        nxt = (cur + 1) if cur + 1 != N else 0
-        j = 0
-        if arr[cur][0] == arr[nxt][0]:
-            j = 1
-        min_p, max_p = sorted([arr[cur][j], arr[nxt][j]])
-        line[min_p + MID] += 1
-        line[max_p + MID] -= 1
+x_list = [0 for _ in range(1000000+1)]
+y_list = [0 for _ in range(1000000+1)]
+x_first,y_first = map(int,input().split())
+x_before, y_before = x_first, y_first
+min_x,min_y = x_first,y_first
+max_x,max_y = x_first,y_first
+for i in range(n-1):
+    x,y = map(int,input().split())
+    min_x,min_y = min(min_x,x) , min(min_y,y)
+    max_x,max_y = max(max_x,x) , max(max_y,y)
+    if x_before==x:
+        y_list[min(y,y_before)]+=1
+        y_list[max(y,y_before)]-=1
+        y_before=y
+    else:
+        x_list[min(x,x_before)]+=1
+        x_list[max(x,x_before)]-=1
+        x_before=x
+if x_before==x_first:
+    y_list[min(y_first,y_before)]+=1
+    y_list[max(y_first,y_before)]-=1
+else:
+    x_list[min(x_first,x_before)]+=1
+    x_list[max(x_first,x_before)]-=1
+        
+# 이제 min_x부터 max_x까지 prefix
+prefix_x = [0 for _ in range(min_x,max_x+1)]
+for x in range(min_x,max_x):
+    prefix_x[x-min_x+1] = prefix_x[x-min_x]+x_list[x]
     
-    ans, tot = 0, 0    
-    for i in line:
-        tot += i
-        ans = max(ans, tot)
-    return ans
+prefix_y = [0 for _ in range(min_y,max_y+1)]
+for y in range(min_y,max_y):
+    prefix_y[y-min_y+1] = prefix_y[y-min_y]+y_list[y]
 
-ans = max(solve(H), solve(V))
-print(ans)
+
+print(max(max(prefix_x),max(prefix_y)))
+
