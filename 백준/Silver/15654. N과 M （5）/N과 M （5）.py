428c2b77197f537f7c5dd25bda9caf2f02792364
galler-ist <37127971+galler-ist@users.noreply.github.com>
2025-01-27 04:32:05 +0900

    [Silver III] Title: N과 M (5), Time: 184 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,18 @@
+def recur():
+
+    if len(arr)==M:
+        print(*arr)
+        return
+    
+    for i in num_list:
+        if i in arr:
+            continue
+        arr.append(i)
+        recur()
+        arr.pop()
+    
+
+N, M = map(int,input().split())
+num_list = sorted(list(map(int,input().split())))
+arr=[]
+recur()
