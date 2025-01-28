bfa3bce49b150de0f9d602d6464d1dbdef76e29f
galler-ist <37127971+galler-ist@users.noreply.github.com>
2025-01-29 03:18:13 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -1,35 +1,17 @@
-import sys
+def recur(idx, S, B):
+    global answer
+    if idx==N:
+        if S==1 and B==0:
+            return
+        temp = abs(S-B)
+        answer = min(temp,answer)
+        return
 
-def food(selected):
-    global result
-    sour=1
-    bitter=0
-    if not selected:return
-    for s,b in selected:
-        sour*=s
-        bitter+=b
-    result = min(result,abs(sour-bitter))
-    return
-
-def make_selected(arr):
-    result_list=[]
-    for i in range(1<<N):
-        subset = []
-        for j in range(N):
-            if i & (1<<j):
-                subset.append(arr[j])
-        result_list.append(subset)
-    return result_list
-
-
+    recur(idx+1,S*ingre[idx][0], B+ingre[idx][1])
+    recur(idx+1,S, B)
+    
 N = int(input())
-#TODO: 신 맛은 사용한 재료의 신맛의 곱, (앞)
-#TODO: 쓴 맛은 사용한 재료의 쓴맛의 합 (뒤)
-arr = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
-result = float('inf')
-selected = make_selected(arr)
-#TODO: selected를 만들기
-#TODO: selected 하나하나 탐색하기
-for select in selected:
-    food(select)
-print(result)
+ingre = [list(map(int,input().split())) for _ in range(N)]
+answer = float('inf')
+recur(0,1,0)
+print(answer)
