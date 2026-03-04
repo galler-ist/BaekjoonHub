43de0a90fe98dbd56c5bc686cbe90f4dc7121ec4
galler-ist <37127971+galler-ist@users.noreply.github.com>
2026-03-05 05:58:40 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
@@ -4,6 +4,8 @@ def dfs():
     while start:
         move,y,x = start.pop()
         for d in dir:
+            if move == 0 :
+                break
             ny,nx = y+d[0]*move, x+d[1]*move
             if ny>=N or nx>=N:
                 continue
@@ -11,16 +13,13 @@ def dfs():
                 print("HaruHaru")
                 return
             else:
-                if not visited[ny][nx]:
-                    visited[ny][nx] = True
-                    start.append((pan[ny][nx],ny,nx))
+                start.append((pan[ny][nx],ny,nx))
     print("Hing")
     return
     
             
 pan = []
 N = int(input())
-visited = [[False] * N for _ in range(N)]
 for _ in range(N):
     pan.append(list(map(int,input().split())))
 dfs()
